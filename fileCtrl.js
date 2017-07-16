e4fe04ea9fe3df25f4f5320746a5e9264c08e33c
Tolga CESUR <ttolgacesur@gmail.com>
2017-07-16 20:01:24 +0300

    file upload controller

---
+++ fileCtrl.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// file for controller
+
+app.controller('fileCtrl',['$scope','$http', function ($scope,$http) {
+ 'use strict';
+	$scope.upload=function () {
+		
+		var fd = new FormData();
+		var files = document.getElementById('file').files[0];
+
+		fd.append('file',files);
+
+		var url = "http://localhost/File-upload-AngularJs-PHP/upload.php";
+
+		//Ajax Request
+	
+		$http({
+			method: 'post',
+			url: url,
+			data: fd,
+			headers: {'Content-Type':undefined},
+		})
+		.then(function Callback(response) {
+
+			//show response
+			// response -> data{name} -> $scope.response
+			$scope.response = response.data;
+			
+		}); 
+
+
+	}
+}]);
+
